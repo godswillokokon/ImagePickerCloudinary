1f08ab81c51792977e8994111c2fd4cf59a073ee
godswillokokon <godswillokokon3@gmail.com>
2020-03-25 12:51:08 +0100

    boilerplate init

--- App.js
+++ App.js
@@ -2,24 +2,22 @@ import React, { useState } from 'react'
 import {
   StyleSheet,
   View,
-  Dimensions,
-  Image,
-  TouchableOpacity,
-  Alert,
   Text
 } from 'react-native';
-// import ImagePicker from 'react-native-image-picker';
+
 
 const App = () => {
   return (
     <View>
-      <Text style={{ alignSelf: 'center', }}>ImagePicker App</Text>
+      <Text style={styles.appText}>ImagePicker App</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  appText: {
+    alignSelf: 'center',
+  }
 });
 
 export default App;

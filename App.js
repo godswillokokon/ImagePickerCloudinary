3a3532a5e61e6ed28ff271f347039b9967d0a0f3
godswillokokon <godswillokokon3@gmail.com>
2020-03-25 15:53:12 +0100

    UI set

--- App.js
+++ App.js
@@ -2,22 +2,75 @@ import React, { useState } from 'react'
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  Image
 } from 'react-native';
 
 
 const App = () => {
   return (
     <View>
-      <Text style={styles.appText}>ImagePicker App</Text>
-    </View>
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: 'https://res.cloudinary.com/ogcodes/image/upload/v1581387688/m0e7y6s5zkktpceh2moq.jpg' }} style={styles.backgroundImage}></Image>
+      </View>
+      <View style={styles.uploadContainer}>
+        <Text style={styles.uploadContainerTitle}>ImagePicker to Cloudinary</Text>
+        <TouchableOpacity style={styles.uploadButton}>
+          <Text style={styles.uploadButtonText}>Upload</Text>
+        </TouchableOpacity>
+      </View>
+
+    </View >
   );
 };
 
 const styles = StyleSheet.create({
-  appText: {
+  imageContainer: {
+    backgroundColor: '#fe5b29',
+    height: Dimensions.get('window').height
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  uploadContainer: {
+    backgroundColor: '#f6f5f8',
+    borderTopLeftRadius: 45,
+    borderTopRightRadius: 45,
+    position: 'absolute',
+    bottom: 0,
+    width: Dimensions.get('window').width,
+    height: 200,
+  },
+  uploadContainerTitle: {
     alignSelf: 'center',
+    fontSize: 30,
+    margin: 20,
+    fontFamily: 'Roboto'
+  },
+  uploadButton: {
+    borderRadius: 16,
+    alignSelf: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 7,
+      height: 5,
+    },
+    shadowOpacity: 1.58,
+    shadowRadius: 9,
+    elevation: 4,
+    margin: 10,
+    padding: 10,
+    backgroundColor: '#fe5b29',
+    width: Dimensions.get('window').width - 60,
+    alignItems: 'center'
+  },
+  uploadButtonText: {
+    color: '#f6f5f8',
+    fontSize: 20,
+    fontFamily: 'Roboto'
   }
 });
-
 export default App;
